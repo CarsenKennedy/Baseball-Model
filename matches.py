d8f1a1b5755363b20dd4ddbdac294d11e368c4fa
Carsen Kennedy <carsenkennedy@pm.me>
2021-10-29 19:50:49 -0700

    Added Game day Schedule function

--- matches.py
+++ matches.py
@@ -1,11 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 from datetime import datetime
 
-date = datetime.today().strftime('%m-%d-%Y')
-dateurl = date.replace('-','')
 
-URL = "https://www.fantasylabs.com/mlb/lineups/?date=" + dateurl
-page = requests.get(URL)
-soup = BeautifulSoup(page.content,"html.parser")
-##games = 
+def get_matches():
+    date = datetime.today().strftime('%Y-%m-%d')
+
+    url = "https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&startDate=" + '2021-10-20' + "&endDate=" + date
+    page = requests.get(url)
+    if page.status_code == 403:
+        return None
+    elif page.status_code == 404:
+        return None
+    else:
+        pass
+    apidata = page.json()
+    apidata['dates']
+    game_list= []
+    for index in apidata['dates']:
+        tuple = (index['games'][0]['teams']['away']['team']['name'],index['games'][0]['teams']['home']['team']['name'])
+        game_list.append(tuple)
+    return game_list
