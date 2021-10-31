0ef4c6cb7920f81bf35e16f3b7bb910f23e76089
Johnathan Cavitt <johnathancavitt@gmail.com>
2021-10-30 18:54:46 -0700

    app.py sends match data with get request

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from matches import *
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
 
-class HelloWorld(Resource):
+class Matches(Resource):
     def get(self):
-        response = {'hello': 'world'}
+        response = get_matches()
         return response
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Matches, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
